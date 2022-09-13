bd7fcf145c9410662cfd616ca1c8ec8ff0e686eb
Javier <javifuego6@gmail.com>
2022-09-13 15:06:26 -0700

    Updated the welcome message

--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -3,9 +3,9 @@ const WelcomeMessage = () => {
     <div className="welcome column">
       <h3> Hi I'm Javier, thank you for visiting my website.</h3>{" "}
       <p>
-        I'm a self taught developer from San Diego looking to fill a position
-        where I can develop sofware and websites. I'm knowledgeable in CSS,
-        HTML, and know basic JavaScript. I'm also familiar with React and I've
+        I'm a self taught coder from San Diego looking to fill a position
+        where I can develop software and websites. I'm knowledgeable in CSS,
+        HTML, and know basic JavaScript. I'm also familiar with frameworks such as React and I've
         built some projects you can check out my work below.
       </p>
     </div>
