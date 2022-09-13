3e03766efe2e5d61ade222d657feef983468dbc9
Javier <javifuego6@gmail.com>
2022-09-13 14:50:43 -0700

    Added new projects to the list and updated the design for smaller screens

--- src/Projects.js
+++ src/Projects.js
@@ -2,6 +2,7 @@ import logo from "./images/imgtwo.png";
 import calculator from "./images/calculator.png";
 import portfolio from "./images/portfolio.png";
 import quoteGenerator from "./images/quotegenerator.png";
+import weatherapp from "./images/weatherapp.png";
 
 const Projects = () => {
   return (
@@ -9,8 +10,14 @@ const Projects = () => {
       <div className="project">
         <div className="column-spaced">
           <h1>Pizzeria Marinara</h1>
-          <p>I built an online menu for a friend's catering business.</p>
-          <a href={"https://github.com/Javier-Ortiz-97/pizzeria-marinara/tree/master"} target="blank">
+          <p>I built a website for a local catering business.</p>
+          <p>Technologies Used: React</p>
+          <a
+            href={
+              "https://github.com/Javier-Ortiz-97/pizzeria-marinara/tree/master/src"
+            }
+            target="blank"
+          >
             <p>Checkout the source code on GitHub</p>
           </a>
           <a href={"https://www.pizzeriamarinarasd.com/"} target="blank">
@@ -18,14 +25,45 @@ const Projects = () => {
           </a>
         </div>
         <div>
-          <img src={logo} alt=""/>
+          <img src={logo} alt="" />
         </div>
       </div>
+
+
+      <div className="project">
+        <div className="column-spaced">
+          <h1>Weather App</h1>
+          <p>
+            I built a weather app where you can type any city and check the
+            current weather.
+          </p>
+          <p>Technologies Used: React, Axios</p>
+          <a
+            href={
+              "https://github.com/Javier-Ortiz-97/weather-app/tree/master/src"
+            }
+            target="blank"
+          >
+            <p>Checkout the source code on GitHub</p>
+          </a>
+          <a href={"https://aesthetic-swan-ff9b6d.netlify.app"} target="blank">
+            <p>View the website live here</p>
+          </a>
+        </div>
+        <div>
+          <img src={weatherapp} alt="" />
+        </div>
+      </div>
+
       <div className="project">
         <div className="column-spaced">
           <h1>Calculator</h1>
-          <p>I built this calculator that performs basic math equations.</p>
-          <a href={"https://github.com/Javier-Ortiz-97/calculator"} target="blank">
+          <p>I built a calculator that performs basic math equations.</p>
+          <p>Technologies Used: React</p>
+          <a
+            href={"https://github.com/Javier-Ortiz-97/calculator/tree/main/src"}
+            target="blank"
+          >
             <p>Checkout the source code on GitHub</p>
           </a>
           <a href={"https://lively-custard-218fb5.netlify.app"} target="blank">
@@ -37,27 +75,40 @@ const Projects = () => {
         </div>
       </div>
       <div className="project">
-      <div className="column-spaced">
-        <h1>Random Quote Generator</h1>
-        <p>I built this random quote generator using React and Axios</p>
-        <a href={"https://github.com/Javier-Ortiz-97/quote-generator/tree/master"} target="blank">
-          <p>Click here to checkout the source code on GitHub</p>
-        </a>
-        <a href={"https://magnificent-chebakia-a6ee7c.netlify.app/"} target="blank">
-          <p>Click here to view the website live</p>
-        </a>
+        <div className="column-spaced">
+          <h1>Random Quote Generator</h1>
+          <p>I built this random quote generator</p>
+          <p>Technologies Used: React, Axios</p>
+          <a
+            href={
+              "https://github.com/Javier-Ortiz-97/quote-generator/tree/master/src"
+            }
+            target="blank"
+          >
+            <p>Checkout the source code on GitHub</p>
+          </a>
+          <a
+            href={"https://magnificent-chebakia-a6ee7c.netlify.app/"}
+            target="blank"
+          >
+            <p>View the website live here</p>
+          </a>
+        </div>
+        <img src={quoteGenerator} alt="" />
       </div>
-      <img src={quoteGenerator} alt="" />
-    </div>
       <div className="project">
         <div className="column-spaced">
           <h1>Portfolio</h1>
           <p>I built this website you are currently viewing</p>
-          <a href={"https://github.com/Javier-Ortiz-97/portfolio"} target="blank">
-            <p>Click here to checkout the source code on GitHub</p>
+          <p>Technologies Used: React</p>
+          <a
+            href={"https://github.com/Javier-Ortiz-97/portfolio/tree/master/src"}
+            target="blank"
+          >
+            <p>Checkout the source code on GitHub</p>
           </a>
           <a href={"https://javierortiz97.netlify.app"} target="blank">
-            <p>Click here to view the website live</p>
+            <p>View the website live here</p>
           </a>
         </div>
         <img src={portfolio} alt="" />
--- src/WelcomeMessage.js
+++ src/WelcomeMessage.js
@@ -3,11 +3,10 @@ const WelcomeMessage = () => {
     <div className="welcome column">
       <h3> Hi I'm Javier, thank you for visiting my website.</h3>{" "}
       <p>
-        I'm a self taught developer from San Diego looking to find a
-        position where I can use my skills to continue to learn and grow as a
-         developer. I am knowledgeable in CSS, HTML, and know basic JavaScript.
-        I'm also familiar with React and I've built some personal projects
-        you can check out my work below.
+        I'm a self taught developer from San Diego looking to fill a position
+        where I can develop sofware and websites. I'm knowledgeable in CSS,
+        HTML, and know basic JavaScript. I'm also familiar with React and I've
+        built some projects you can check out my work below.
       </p>
     </div>
   );
